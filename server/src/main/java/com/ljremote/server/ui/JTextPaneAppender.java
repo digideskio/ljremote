e5c0733889edacfb3a3a5fb1a56e26e20d3d1c06
zeenlym <ylisima@TLS-231>
2012-12-17 15:26:49 +0100

    Manage Font for logging

--- server/src/main/java/com/ljremote/server/ui/JTextPaneAppender.java
+++ server/src/main/java/com/ljremote/server/ui/JTextPaneAppender.java
@@ -1,45 +1,89 @@
 package com.ljremote.server.ui;
 
+import java.awt.Color;
+
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
 public class JTextPaneAppender extends AppenderSkeleton {
-	
+
 	private static JTextPane textPane;
 	private static StyledDocument doc;
+	private static Style styleBase;
+	private static Style styleInfo;
+	private static Style styleWarn;
+	private static Style styleFatal;
+	private static Style styleTrace;
+	private static Style styleDebug;
+	private static Style styleError;
 
-	public JTextPaneAppender(){
-		if(doc == null){
-			textPane= new JTextPane();
+	public JTextPaneAppender() {
+		if (doc == null) {
+			textPane = new JTextPane();
 			textPane.setEditable(false);
-			doc= textPane.getStyledDocument();
+			doc = textPane.getStyledDocument();
+
+			styleBase = doc.addStyle("base", null);
+			StyleConstants.setFontFamily(styleBase, "monospaced");
+
+			styleInfo = doc.addStyle("info", styleBase);
+			StyleConstants.setForeground(styleInfo, Color.BLUE);
+
+			styleDebug = doc.addStyle("debug", styleBase);
+			StyleConstants.setForeground(styleDebug, Color.GREEN);
+
+			styleWarn = doc.addStyle("warn", styleBase);
+			StyleConstants.setForeground(styleWarn, Color.ORANGE);
+
+			styleError = doc.addStyle("error", styleBase);
+			StyleConstants.setForeground(styleError, Color.RED);
+
+			styleFatal = doc.addStyle("fatal", styleError);
+			StyleConstants.setBold(styleFatal, true);
+
+			styleTrace = doc.addStyle("trace", styleBase);
+			StyleConstants.setItalic(styleTrace, true);
 		}
 	}
-	
-	
-	
+
 	@Override
 	public void activateOptions() {
 		super.activateOptions();
 	}
 
-
-
 	@Override
-	protected void append(LoggingEvent arg0) {
-		final String message = layout.format(arg0);
-		
+	protected void append(LoggingEvent event) {
+		final String message = layout.format(event);
+		final Style style;
+		if (event.getLevel().equals(Level.INFO)) {
+			style = styleInfo;
+		} else if (event.getLevel().equals(Level.DEBUG)) {
+			style = styleDebug;
+		} else if (event.getLevel().equals(Level.WARN)) {
+			style = styleWarn;
+		} else if (event.getLevel().equals(Level.ERROR)) {
+			style = styleError;
+		} else if (event.getLevel().equals(Level.FATAL)) {
+			style = styleFatal;
+		} else if (event.getLevel().equals(Level.TRACE)) {
+			style = styleTrace;
+		} else {
+			style = styleBase;
+		}
 		SwingUtilities.invokeLater(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				try {
-					doc.insertString(doc.getLength(), message, null);
+					doc.insertString(doc.getLength(), message, style);
 				} catch (BadLocationException e) {
 					e.printStackTrace();
 				}
@@ -49,7 +93,7 @@ public class JTextPaneAppender extends AppenderSkeleton {
 
 	@Override
 	public void close() {
-		super.closed= true;
+		super.closed = true;
 	}
 
 	@Override
@@ -57,16 +101,16 @@ public class JTextPaneAppender extends AppenderSkeleton {
 		return true;
 	}
 
-	public JTextPane getView(){
+	public JTextPane getView() {
 		return textPane;
 	}
-	
+
 	/**
 	 * @wbp.factory
 	 * @return
 	 */
-	public static JTextPane createJTextPaneAppender(){
+	public static JTextPane createJTextPaneAppender() {
 		return new JTextPaneAppender().getView();
 	}
-	
+
 }
